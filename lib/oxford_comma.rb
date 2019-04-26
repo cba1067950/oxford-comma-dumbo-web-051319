6d5558d58093ae6ad52a810c6fd3844c19b75cfc
Joe Jamsky <cba1067950@yahoo.com>
2019-04-26 00:09:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,19 +6,16 @@ def oxford_comma(array)
   #returns string without formatting if 1 element
   when capacity == 1 
     array.join
+    
   #adds and for 2 elements  
   when capacity == 2 
     array.join(" and ")
   
   #adds commas and final and for 3 elements
-  #kiwi durian starfruit
-  #kiwi, durian, and starfruit 
-  #kiwi, durian, starfruit
   when capacity >= 3 
     array.pop()
     array.push("and ")
     newString = array.join(", ")
     newString << lastElement
-    newString
   end
 end
