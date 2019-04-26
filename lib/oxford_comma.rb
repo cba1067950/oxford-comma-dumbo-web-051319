de76a627cad31e0d0718ef4aaf95d185cf96d53b
Joe Jamsky <cba1067950@yahoo.com>
2019-04-26 00:04:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -16,7 +16,7 @@ def oxford_comma(array)
   #kiwi, durian, starfruit
   when capacity == 3 
     string1 = array.join(", ")
-    array2 = string1.split(", ")
+    array2 = string1.split("")
     array.insert(array.length-1, " and ")
     array.join()
     
